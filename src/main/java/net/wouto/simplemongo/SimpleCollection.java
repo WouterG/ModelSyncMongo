ea1f7fead06c3c5c327beddb1ea993ff1b77805c
Wouter Gerarts <wouto1997@gmail.com>
2015-05-29 22:04:02 +0200

    Reflection

--- src/main/java/net/wouto/simplemongo/SimpleCollection.java
+++ src/main/java/net/wouto/simplemongo/SimpleCollection.java
@@ -1,5 +1,6 @@
 package net.wouto.simplemongo;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.mongodb.client.MongoCollection;
@@ -7,12 +8,18 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
+import com.mongodb.util.JSON;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.wouto.simplemongo.annotations.DBSync;
@@ -26,6 +33,7 @@ import net.wouto.simplemongo.query.Query;
 import net.wouto.simplemongo.sync.ObjectLoadedCallback;
 import net.wouto.simplemongo.update.Update;
 import org.apache.commons.lang.ClassUtils;
+import org.bson.Document;
 
 public class SimpleCollection {
 
@@ -276,7 +284,7 @@ public class SimpleCollection {
         MongoCursor cursor = this.findSync(Query.empty);
         ArrayList<T> data = new ArrayList();
         while (cursor.hasNext()) {
-            DBObject obj = (DBObject) cursor.next();
+            DBObject obj = (DBObject) JSON.parse(((Document) cursor.next()).toJson());
             T instance = (T) constructor.newInstance();
             SimpleCollection.this.fromDBObject(instance, obj);
             data.add(instance);
@@ -303,42 +311,155 @@ public class SimpleCollection {
         });
     }
 
-    public final <T> void save(T instance) {
-        Query index = new Query();
-        Update update = new Update();
-        String key = null;
-        Object value = null;
-        Field[] fields = instance.getClass().getDeclaredFields();
-        for (Field f : fields) {
-            try {
-                if (!f.isAnnotationPresent(DBSync.class)) {
-                    continue;
+    private static Collection<Field> getAnnotationFields(Class<?> clazz, Class<? extends Annotation> a) {
+        List<Field> fields = new ArrayList();
+        while (clazz != null && clazz != Object.class) {
+            Field[] fs = clazz.getDeclaredFields();
+            for (Field f : fs) {
+                if (f.isAnnotationPresent(a)) {
+                    fields.add(f);
                 }
+            }
+            clazz = clazz.getSuperclass();
+        }
+        return fields;
+    }
+
+    private static final Set<Class<?>> primitives = new HashSet<Class<?>>() {
+        {
+            add(Boolean.class);
+            add(Character.class);
+            add(Byte.class);
+            add(Short.class);
+            add(Integer.class);
+            add(Long.class);
+            add(Float.class);
+            add(Double.class);
+            add(Object.class);
+            add(String.class);
+        }
+    };
+
+    private static Class<?> getCollectionType(Field f) {
+        return (Class<?>) ((ParameterizedType) f.getGenericType()).getActualTypeArguments()[0];
+    }
+
+    private static Class<?> getArrayType(Field f) {
+        return f.getType().getComponentType();
+    }
+
+    private static boolean isSerializeAble(Field f) {
+        Class<?> type = f.getType();
+        if (type.isArray()) {
+            type = type.getComponentType();
+        }
+        if (Collection.class.isAssignableFrom(f.getType())) {
+            type = getCollectionType(f);
+        }
+        if (isPrimitive(type)) {
+            return true;
+        }
+        return (!getAnnotationFields(type, DBSync.class).isEmpty());
+    }
+
+    private static boolean isPrimitive(Class<?> type) {
+        if (type.isPrimitive()) {
+            return true;
+        }
+        for (Class<?> p : primitives) {
+            if (type.isAssignableFrom(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static <T> BasicDBObject asDBObject(T instance) {
+        BasicDBObject b = new BasicDBObject();
+        Collection<Field> fieldMap = getAnnotationFields(instance.getClass(), DBSync.class);
+        for (Field f : fieldMap) {
+            try {
                 DBSync saveData = f.getAnnotation(DBSync.class);
                 if (saveData == null) {
+                    continue;
+                }
+                if (!isSerializeAble(f)) {
                     continue;
                 }
                 boolean hadAccess = f.isAccessible();
                 if (!hadAccess) {
                     f.setAccessible(true);
                 }
-                Object o = f.get(instance);
                 String fieldName = f.getName();
                 if (!saveData.value().isEmpty()) {
                     fieldName = saveData.value();
                 }
-                f.setAccessible(hadAccess);
-                if (saveData.index()) {
-                    key = fieldName;
-                    value = o;
-                    index.append(Query.equals(fieldName, o));
-                } else if (!fieldName.equals("_id")) {
-                    update.append(Update.set(fieldName, o));
+                if (f.getType().isArray()) { // array
+                    Object[] arrayData = (Object[]) f.get(instance);
+                    Class<?> arrayType = getArrayType(f);
+                    if (isPrimitive(arrayType)) {
+                        b.put(fieldName, f.get(instance));
+                        f.setAccessible(hadAccess);
+                        continue;
+                    } else {
+                        BasicDBList list = new BasicDBList();
+                        for (Object arrayObject : arrayData) {
+                            list.add(asDBObject(arrayObject));
+                        }
+                        b.put(fieldName, list);
+                        f.setAccessible(hadAccess);
+                        continue;
+                    }
+                } else if (Collection.class.isAssignableFrom(f.getType())) { // Collection
+                    Collection collectionData = (Collection) f.get(instance);
+                    Class<?> collectionType = getCollectionType(f);
+                    if (isPrimitive(collectionType)) {
+                        b.put(fieldName, f.get(instance));
+                        f.setAccessible(hadAccess);
+                        continue;
+                    } else {
+                        BasicDBList list = new BasicDBList();
+                        for (Object collectionObject : collectionData) {
+                            BasicDBObject collectionDBObject = asDBObject(collectionType.cast(collectionObject));
+                            list.add(collectionDBObject);
+                        }
+                        b.put(fieldName, list);
+                        f.setAccessible(hadAccess);
+                        continue;
+                    }
+                } else if (!isPrimitive(f.getType())) {
+                    BasicDBObject data = asDBObject(f.get(instance));
+                    b.put(fieldName, data);
+                    f.setAccessible(hadAccess);
+                    continue;
                 }
+                Object o = f.get(instance);
+                f.setAccessible(hadAccess);
+                b.put(fieldName, o);
             } catch (IllegalAccessException | IllegalArgumentException ex) {
                 ex.printStackTrace();
             }
         }
+        return b;
+    }
+
+    public final <T> void save(T instance) {
+        BasicDBObject obj = asDBObject(instance);
+        Collection<Field> fields = getAnnotationFields(instance.getClass(), DBSync.class);
+        String key = null;
+        Object value = null;
+        for (Field f : fields) {
+            if (f.getAnnotation(DBSync.class).index()) {
+                key = f.getName();
+                value = obj.remove(f.getName());
+                break;
+            }
+        }
+        if (key == null && value == null) {
+            System.out.println("Cannot save object: " + instance.getClass().getName());
+            return;
+        }
+        Update update = new Update(obj);
         this.updateOrInsert(key, value, update);
     }
 
@@ -348,7 +469,7 @@ public class SimpleCollection {
         if (!a) {
             constr.setAccessible(true);
         }
-        final List<Object> params = new ArrayList<Object>();
+        final List<Object> params = new ArrayList();
         for (Class<?> pType : constr.getParameterTypes()) {
             params.add((pType.isPrimitive()) ? ClassUtils.primitiveToWrapper(pType).newInstance() : null);
         }
@@ -357,7 +478,7 @@ public class SimpleCollection {
         return instance;
     }
 
-    private final <T> T fromDBObject(Class<T> type, DBObject data) {
+    private <T> T fromDBObject(Class<T> type, DBObject data) {
         try {
             T obj = (T) instantiate(type);
             return fromDBObject(obj, data);
@@ -369,23 +490,84 @@ public class SimpleCollection {
         return null;
     }
 
-    private final <T> T fromDBObject(T instance, DBObject data) {
-        Field[] fields = instance.getClass().getDeclaredFields();
+    private <T> T fromDBObject(T instance, DBObject data) {
+        Collection<Field> fields = getAnnotationFields(instance.getClass(), DBSync.class);
         for (Field f : fields) {
-            if (!f.isAnnotationPresent(DBSync.class)) {
-                continue;
-            }
             DBSync load = f.getAnnotation(DBSync.class);
             if (load == null) {
+                continue;
+            }
+            String varName = f.getName();
+            if (!load.value().isEmpty()) {
+                varName = load.value();
+            }
+            if (!data.containsField(varName)) {
                 continue;
             }
             boolean accessible = f.isAccessible();
             if (!accessible) {
                 f.setAccessible(true);
             }
-            String varName = f.getName();
-            if (!load.value().isEmpty()) {
-                varName = load.value();
+            if (f.getType().isArray()) { // array
+                Class<?> arrayType = getArrayType(f);
+                if (isPrimitive(arrayType)) {
+                    try {
+                        f.set(instance, data.get(varName));
+                    } catch (IllegalArgumentException | IllegalAccessException ex) {
+                        Logger.getLogger(SimpleCollection.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    f.setAccessible(accessible);
+                    continue;
+                }
+                BasicDBList arrayData = (BasicDBList) data.get(varName);
+                Collection arrayResult = new ArrayList();
+                for (Object arrayObject : arrayData) {
+                    DBObject arrayObjectDB = (DBObject) arrayObject;
+                    Object arrayObjectOutput = fromDBObject(arrayType, arrayObjectDB);
+                    arrayResult.add(arrayObjectOutput);
+                }
+                try {
+                    f.set(instance, arrayResult.toArray());
+                } catch (IllegalArgumentException | IllegalAccessException ex) {
+                    Logger.getLogger(SimpleCollection.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                f.setAccessible(accessible);
+                continue;
+            } else if (Collection.class.isAssignableFrom(f.getType())) { // collection
+                Class<?> collectionType = getCollectionType(f);
+                if (isPrimitive(collectionType)) {
+                    try {
+                        f.set(instance, data.get(varName));
+                    } catch (IllegalArgumentException | IllegalAccessException ex) {
+                        Logger.getLogger(SimpleCollection.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    f.setAccessible(accessible);
+                    continue;
+                }
+                BasicDBList collectionData = (BasicDBList) data.get(varName);
+                Collection collectionResult = new ArrayList();
+                for (Object collectionObject : collectionData) {
+                    DBObject collectionObjectDB = (DBObject) collectionObject;
+                    Object collectionObjectOutput = fromDBObject(collectionType, collectionObjectDB);
+                    collectionResult.add(collectionObjectOutput);
+                }
+                try {
+                    f.set(instance, collectionResult);
+                } catch (IllegalArgumentException | IllegalAccessException ex) {
+                    Logger.getLogger(SimpleCollection.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                f.setAccessible(accessible);
+                continue;
+            } else if (!isPrimitive(f.getType())) {
+                Object o = fromDBObject(f.getType(), (DBObject) data.get(varName));
+                try {
+                    f.set(instance, o);
+                } catch (IllegalArgumentException | IllegalAccessException ex) {
+                    Logger.getLogger(SimpleCollection.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                f.setAccessible(accessible);
+                continue;
             }
             Object o = data.get(varName);
             try {
@@ -453,35 +635,6 @@ public class SimpleCollection {
             }
 
         });
-    }
-
-    public static <T> BasicDBObject asDBObject(T instance) {
-        BasicDBObject b = new BasicDBObject();
-        Field[] fields = instance.getClass().getDeclaredFields();
-        for (Field f : fields) {
-            try {
-                DBSync saveData = f.getAnnotation(DBSync.class);
-                if (saveData == null) {
-                    continue;
-                }
-                boolean accessible = f.isAccessible();
-                if (!accessible) {
-                    f.setAccessible(true);
-                }
-                String varName = f.getName();
-                if (!saveData.value().isEmpty()) {
-                    varName = saveData.value();
-                }
-                Object o = f.get(instance);
-                if (!saveData.value().equals("_id")) {
-                    b.put(varName, o);
-                }
-                f.setAccessible(accessible);
-            } catch (IllegalAccessException | IllegalArgumentException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return b;
     }
 
     public MongoCollection getHandle() {
